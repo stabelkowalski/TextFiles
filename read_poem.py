3ca07b424929b1a3960ba47e9a6cb79f7574bded
stabelkowalski <stabelkowalski@gmail.com>
2023-07-12 20:55:04 +0200

    2 day

--- read_poem.py
+++ read_poem.py
@@ -7,6 +7,34 @@
 #
 # jabber.close()
 
-with open('Jabberwocky.txt', 'r') as jabber:
+# with open('Jabberwocky.txt', 'r') as jabber:
+# #     for line in jabber:
+# #         print(line.rstrip())
+#     lines = jabber.readlines()
+#
+# print(lines)
+# print(lines[-1:])
+#
+# for line in reversed(lines):
+#     print(line, end='')
+
+# with open('Jabberwocky.txt') as jabber:
+#     text = jabber.read()
+#
+# print(text)
+# for character in reversed(text):
+#     print(character, end='')
+
+with open('Jabberwocky.txt') as jabber:
+    while True:
+        line = jabber.readline().rstrip()
+        print(line)
+        if 'jubjub' in line.casefold():
+            break
+print('x' * 80)
+
+with open('Jabberwocky.txt') as jabber:
     for line in jabber:
         print(line.rstrip())
+        if 'jubjub' in line.casefold():
+            break
---
+++ stripping.py
@@ -0,0 +1,49 @@
+def removeprefix(string: str, prefix: str) -> str:
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    else:
+        return string[:]
+
+
+def removesuffix(string: str, suffix: str) -> str:
+    if suffix and string.endswith(suffix): # suffix='' should not call string[:-0]
+        return string[:-len(suffix)]
+    else:
+        return string[:]
+
+
+
+filename = 'Jabberwocky.txt'
+
+with open(filename) as poem:
+    first = poem.readline().rstrip()
+
+print(first)
+
+chars = "' Twasebv"
+# no_apostrophe = first.strip(chars)
+# print(no_apostrophe)
+
+for character in first:
+    if character in chars:
+        print(f'removing "{character}"')
+    else:
+        break
+
+print('x' * 80)
+
+for character in reversed(first): # albo first[::-1]
+    if character in chars:
+        print(f'removing "{character}"')
+    else:
+        break
+
+
+print('x' * 80)
+
+# twas_removed = first.removeprefix("'Twas")
+twas_removed = removeprefix(first, "'Twas")
+print(twas_removed)
+# toves_removed = first.removesuffix("toves")
+toves_removed = removesuffix(first, 'toves')
+print(toves_removed)
