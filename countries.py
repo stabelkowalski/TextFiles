f94f83c096dfd07eae1653f108b3c5d73313c3b7
stabelkowalski <stabelkowalski@gmail.com>
2023-08-01 22:00:06 +0200

    d

--- countries.py
+++ countries.py
@@ -1,6 +1,8 @@
 input_filename = 'country_info.txt'
 
 countries = {}
+code_lookup = {}
+no_capital = []
 with open(input_filename) as country_file:
     country_file.readline()
     for row in country_file:
@@ -18,16 +20,28 @@ with open(input_filename) as country_file:
         }
         # print(country_dict)
         countries[country.casefold()] = country_dict
+        # code_lookup[code.casefold()] = country
+        # countries[code.casefold()] = country_dict
 
-# print(countries)
+
+print(countries)
+# print(code_lookup)
+
+# while True:
+#     chosen_country = input('Please enter the name of a country: ').casefold()
+#     if chosen_country in countries:
+#         country_data = countries[chosen_country]
+#         print(f"The capital of {country_data['name']} is {country_data['capital']}")
+#     elif chosen_country == 'quit':
+#         break
+#     elif chosen_country not in countries:
+#         print(f"There is no country in data set as {chosen_country}")
+
+for key, value in countries.items():
+    if value['capital'] == '':
+        no_capital.append(value['name'])
+print(no_capital)
 
 
-while True:
-    chosen_country = input('Please enter the name of a country: ').casefold()
-    if chosen_country in countries:
-        country_data = countries[chosen_country]
-        print(f"The capital of {chosen_country} is {country_data['capital']}")
-    elif chosen_country not in countries:
-        print(f"There is no country in data set as {chosen_country}")
-    elif chosen_country == 'quit':
-        break
+
+
